b32ded09baa521828e21fa7742a5768122094c31
DNI9 <itsr9boi@gmail.com>
2022-01-15 01:25:13 +0530

    update scripts

--- .dwm/bin/setwallpaper.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-choice=$(sxiv -rto /usr/share/backgrounds/)
-echo $choice
-if [[ ! $choice ]]; then
-	echo "Choose an image to set as the wallpaper"; exit 1
-else
-	echo "setting wallpaper"
-	nitrogen --set-scaled $choice --save
-fi
--- PKGBUILD
+++ PKGBUILD
@@ -1,5 +1,5 @@
 pkgname=dni9-dwm
-pkgver=1.r1.5dd0a2e
+pkgver=1.r4.d0f3ce2
 pkgrel=1
 pkgdesc="My bloated build of DWM"
 url="https://github.com/DNI9/dwm"
