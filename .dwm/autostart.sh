14c0a46e831aa73dc174ff95507c7501fcef6827
DNI9 <itsr9boi@gmail.com>
2022-01-15 00:40:23 +0530

    update autostart script

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
 
-# wmname LG3D &
-#change your keyboard if you need it
-#setxkbmap -layout be
-#swich around capslock and escape key
+wmname LG3D &
 #xmodmap -e "keycode 9 = Caps_Lock"; xmodmap -e "keycode 66 = Escape" &
 #xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape' &
-# killall mocicon
-# mocicon &
 
-# killall volumeicon
-# volumeicon &
+killall volumeicon
+volumeicon &
 
-hsetroot -cover "$(find ~/Pictures -type f -name '*.png' | shuf -n 1)"
+hsetroot -cover "$(find ~/Pictures/wallpapers/ -type f -name '*' | shuf -n 1)"
 xsetroot -cursor_name left_ptr &
-# nm-applet &
-# numlockx on &
+nm-applet &
+numlockx on &
 # uncomment the 3 lines below to start bluetooth on boot
 #killall blueberry
 #killall blueberry-tray
 #blueberry-tray &
+
 killall picom
 picom --experimental-backends --config ~/.dwm/picom.conf &
 
@@ -36,5 +32,5 @@ dwmblocks &
 killall sxhkd
 sxhkd -c ~/.dwm/sxhkdrc &
 
-# killall betterlockscreen
-# betterlockscreen -u "$(cat ~/.config/nitrogen/bg-saved.cfg | grep file | sed 's/file=//g')" &
+killall betterlockscreen
+betterlockscreen -u "$(find ~/Pictures/wallpapers/ -type f -name '*' | shuf -n 1)" &
