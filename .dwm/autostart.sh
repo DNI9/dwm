9f7fa8022d017774619991baedb517a134bf45a2
DNI9 <itsr9boi@gmail.com>
2022-01-15 18:40:53 +0530

    update config

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,9 +4,6 @@ wmname LG3D &
 #xmodmap -e "keycode 9 = Caps_Lock"; xmodmap -e "keycode 66 = Escape" &
 #xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape' &
 
-killall volumeicon
-volumeicon &
-
 hsetroot -cover "$(find ~/Pictures/wallpapers/ -type f -name '*' | shuf -n 1)"
 xsetroot -cursor_name left_ptr &
 nm-applet &
